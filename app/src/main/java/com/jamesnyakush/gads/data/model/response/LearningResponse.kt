6e0afc76af6344877fd369752a7b36cb89377dfe
jamesnyakush <emvesttech@gmail.com>
2020-09-06 05:15:46 +0300

    Tested Retrofit :rocket: :relaxed:

---
+++ app/src/main/java/com/jamesnyakush/gads/data/model/response/LearningResponse.kt
@@ -0,0 +1,5 @@
+package com.jamesnyakush.gads.data.model.response
+
+import com.jamesnyakush.gads.data.model.Learning
+
+class LearningResponse : ArrayList<Learning>()
--- app/src/main/java/com/jamesnyakush/gads/ui/activity/Leaderboard.kt
+++ app/src/main/java/com/jamesnyakush/gads/ui/activity/Leaderboard.kt
@@ -3,11 +3,28 @@ package com.jamesnyakush.gads.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jamesnyakush.gads.R
+import com.jamesnyakush.gads.ui.fragment.Learning
+import com.jamesnyakush.gads.ui.fragment.SkillIQ
+import com.jamesnyakush.gads.utils.ViewPagerAdapter
+import kotlinx.android.synthetic.main.leaderboard_activity.*
 
 class Leaderboard : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.leaderboard_activity)
+
+        setup()
+    }
+
+    private fun setup() {
+        val adapter = ViewPagerAdapter(
+            supportFragmentManager
+        )
+        adapter.addFragment(Learning(), "Learning Leaders")
+        adapter.addFragment(SkillIQ(), "Skill IQ Leaders")
+
+        pager.adapter = adapter
+        tabs.setupWithViewPager(pager)
     }
 }
