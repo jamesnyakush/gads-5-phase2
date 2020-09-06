3c0ba7ac2babec441405017d864fdfce105407d0
jamesnyakush <emvesttech@gmail.com>
2020-09-06 05:41:38 +0300

    Fixed Retrofit :relaxed: :rocket:

---
+++ app/src/main/java/com/jamesnyakush/gads/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.jamesnyakush.gads.utils
+
+object Constants {
+    const val HEROKU_URL: String = "https://gadsapi.herokuapp.com/"
+    const val GOOGLE_FORM_URL: String = "https://gadsapi.herokuapp.com/"
+
+}
