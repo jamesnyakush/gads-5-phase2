26c28212f21618e7d6fcaaf912b97e5afc47daa5
jamesnyakush <emvesttech@gmail.com>
2020-09-06 03:29:02 +0300

    Added adapters :rocket: :relaxed:

---
+++ app/src/main/java/com/jamesnyakush/gads/data/model/Learning.kt
@@ -0,0 +1,8 @@
+package com.jamesnyakush.gads.data.model
+
+data class Learning(
+    val badgeUrl: String,
+    val country: String,
+    val hours: Int,
+    val name: String
+)
--- app/src/main/java/com/jamesnyakush/gads/ui/adapter/SkillIQAdapter.kt
+++ app/src/main/java/com/jamesnyakush/gads/ui/adapter/SkillIQAdapter.kt
@@ -1,2 +1,25 @@
 package com.jamesnyakush.gads.ui.adapter
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.jamesnyakush.gads.R
+import com.jamesnyakush.gads.data.model.SkillIQ
+
+class SkillIQAdapter(
+    var skills: List<SkillIQ>
+) : RecyclerView.Adapter<SkillIQAdapter.SkillIQViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = SkillIQViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_skill, parent, false)
+    )
+
+    override fun getItemCount() = skills.size
+
+    override fun onBindViewHolder(holder: SkillIQViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    inner class SkillIQViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
